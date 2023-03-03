ba756ce8bb4df4e341ae9461604a77d74870ab02
dheerajkrishna458 <dheerajkittu27@gmail.com>
2023-03-02 21:31:39 -0600

    dheerj krishna mekala -- reservationsuccess layout error correction completed

--- app/src/main/java/com/example/bookmylotnwm/MainActivity.java
+++ app/src/main/java/com/example/bookmylotnwm/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.bookmylotnwm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+    public void signin(View v){
+        setContentView(R.layout.type);
+    }
+    public void next1(View v){
+        setContentView(R.layout.typeofvehicle);
+    }
+    public void next2(View v){
+        setContentView(R.layout.parkingplace);
+    }
+    public void parkingview(View v){
+        setContentView(R.layout.parkingview);
+        TextView tv= (TextView) findViewById(R.id.ParkingName);
+        tv.setText("Library Parking");
+    }
+    public void reserve(View v){
+        setContentView(R.layout.reservation);
+    }
+    public void success(View v){
+        setContentView(R.layout.reservesuccess);
     }
 }
