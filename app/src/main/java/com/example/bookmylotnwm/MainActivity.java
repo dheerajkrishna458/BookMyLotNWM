c985398e8f4cfcc08b6cf6c90b340e70f43f6363
susmitha muppalla <112180109+SusmithaMuppalla@users.noreply.github.com>
2023-03-24 21:21:21 -0500

    s555460 susmitha muppalla
     for new_user_registration java code has been written

--- app/src/main/java/com/example/bookmylotnwm/MainActivity.java
+++ app/src/main/java/com/example/bookmylotnwm/MainActivity.java
@@ -75,4 +75,53 @@ public class MainActivity extends AppCompatActivity {
             setContentView(R.layout.checkoutunsuccessful);
         }
 
+    }
+    public void signup(View v){
+        setContentView(R.layout.new_user_registration);
+        EditText username, password, repassword;
+        Button signup, signin;
+        DBHelper DB;
+        username = (EditText) findViewById(R.id.usernamenew);
+        password = (EditText) findViewById(R.id.passwordnew);
+        repassword = (EditText) findViewById(R.id.repasswordnew);
+        signup = (Button) findViewById(R.id.register);
+        signin = (Button) findViewById(R.id.backtosignin);
+        DB = new DBHelper(this);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user = username.getText().toString();
+                String pass = password.getText().toString();
+                String repass = repassword.getText().toString();
+
+                if(user.equals("")||pass.equals("")||repass.equals(""))
+                    Toast.makeText(MainActivity.this, "Please enter all the fields", Toast.LENGTH_SHORT).show();
+                else{
+                    if(pass.equals(repass)){
+                        Boolean checkuser = DB.checkusername(user);
+                        if(checkuser==false){
+                            Boolean insert = DB.insertData(user, pass);
+                            if(insert==true){
+                                Toast.makeText(MainActivity.this, "Registered successfully", Toast.LENGTH_SHORT).show();
+                                setContentView(R.layout.new_reservation);
+                            }else{
+                                Toast.makeText(MainActivity.this, "Registration failed", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                        else{
+                            Toast.makeText(MainActivity.this, "User already exists! please sign in", Toast.LENGTH_SHORT).show();
+                        }
+                    }else{
+                        Toast.makeText(MainActivity.this, "Passwords not matching", Toast.LENGTH_SHORT).show();
+                    }
+                } }
+        });
+
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.activity_main);
+            }
+        });
     }}
